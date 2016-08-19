c987fd1bc9c1429c671bd3e4cd7dab3a437c95ef
Jacqui Ahn <jsahn23@gmail.com>
2016-08-19 10:26:17 -0400

    adds crap

--- js/compliments/compliments.controller.js
+++ js/compliments/compliments.controller.js
@@ -1,11 +1,13 @@
 `use strict`;
 
 (function(){
-  var app = angular.module("compliments");
+  var app = angular.module("angularComp");
   app.controller("complimentsController", ComplimentsController);
 
   function ComplimentsController(){
     var vm = this;
+    var randomIndex = Math.floor(Math.random()* data.length)
+    vm.data = data[randomIndex]
   }
 
 }());
